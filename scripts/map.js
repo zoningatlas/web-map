7bf4d74a866f219bdc37c1b9429620bfdc168d40
kmal808 <kjmalley7@gmail.com>
2022-10-17 23:51:19 -1000

    task: adding alternate page load view

--- scripts/map.js
+++ scripts/map.js
@@ -468,7 +468,7 @@ var initMap = function () {
 		zoomControl: false,
 		tap: false,
 		maxZoom: 15,
-	}).setView([21.427, -158.0203], 11)
+	}).setView([19.961, -158.421], 8)
 
 	L.control.zoom({ position: 'topright' }).addTo(map)
 
